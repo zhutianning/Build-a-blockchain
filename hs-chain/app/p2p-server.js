9649c58f0bd0049359be9117c88305db12b5289b
zhutianning <tianning.zhu@kcl.ac.uk>
2019-04-17 18:29:33 +0100

    handle message from peers

--- hs-chain/app/p2p-server.js
+++ hs-chain/app/p2p-server.js
@@ -29,8 +29,18 @@ class P2pServer {
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHanlder(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
     }
 
+    messageHanlder(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data', data);
+        });
+    }
 }
 
 module.exports = P2pServer;
