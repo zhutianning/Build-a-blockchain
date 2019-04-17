f4af62f3cb89a76dead136037af9f54a8890ba5d
zhutianning <tianning.zhu@kcl.ac.uk>
2019-04-17 19:08:57 +0100

    Synchronize the blockchain across peers

--- hs-chain/app/p2p-server.js
+++ hs-chain/app/p2p-server.js
@@ -31,15 +31,23 @@ class P2pServer {
         console.log('Socket connected');
 
         this.messageHanlder(socket);
-
-        socket.send(JSON.stringify(this.blockchain.chain));
+        this.sendChain(socket);
     }
 
     messageHanlder(socket) {
         socket.on('message', message => {
             const data = JSON.parse(message);
-            console.log('data', data);
+
+            this.blockchain.replaceChain(data);
         });
+    }
+
+    sendChain(socket) {
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    syncChains() {
+        this.sockets.forEach(socket => this.sendChain(socket));
     }
 }
 
